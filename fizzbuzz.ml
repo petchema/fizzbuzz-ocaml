87d9af6315ebfd9d217ef91cb353aae82f764cf7
Pierre Etchemaite <petchema@concept-micro.com>
2012-07-23 21:21:30 +0200

    Test 5 passes

--- fizzbuzz.ml
+++ fizzbuzz.ml
@@ -1,3 +1,4 @@
 let game x =
   if x mod 3 = 0 then "Fizz"
+  else if x mod 5 = 0 then "Buzz"
   else string_of_int x
--- testfizzbuzz.ml
+++ testfizzbuzz.ml
@@ -10,11 +10,15 @@ let test_two () =
 let test_three () =
   assert_equal (game 3) "Fizz"
 
+let test_five () =
+  assert_equal (game 5) "Buzz"
+
 let suite = "FizzBuzz test suite" >::: [
 (* begin suite *)
   "test_one" >:: test_one;
   "test_two" >:: test_two;
   "test_three" >:: test_three;
+  "test_five" >:: test_five;
 (* end suite *)
 ]
 
