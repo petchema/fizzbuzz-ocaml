1faeecf65ee1f44a3f148afc0a7e619257244a15
Pierre Etchemaite <petchema@concept-micro.com>
2012-07-23 20:56:31 +0200

    Test 3 passes

--- fizzbuzz.ml
+++ fizzbuzz.ml
@@ -1,7 +1,9 @@
 open OUnit
 
 let game x =
-  if x = 1 then "1" else "2"
+  if x = 1 then "1" 
+  else if x = 2 then "2"
+  else "Fizz"
 
 let test_one () =
   assert_equal (game 1) "1"
@@ -9,9 +11,13 @@ let test_one () =
 let test_two () =
   assert_equal (game 2) "2"
 
+let test_three () =
+  assert_equal (game 3) "Fizz"
+
 let suite = "FizzBuzz test suite" >::: [
   "test_one" >:: test_one;
   "test_two" >:: test_two;
+  "test_three" >:: test_three;
 ]
 
 let _ =
